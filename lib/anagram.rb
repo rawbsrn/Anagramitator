43c8653d573ad200d35c2261ceebed4b392f63b9
Rob Sameh <rob.sameh@gmail.com>
2022-04-23 18:17:18 -0700

    wrote and passing test two

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,7 +1,7 @@
 class Anagram < String
   def sort(input1, input2)
-  @input1 = input1
-  @input2 = input2
+  @input1 = input1.downcase
+  @input2 = input2.downcase
   sorted1 = @input1.chars.sort.join
   sorted2 = @input2.chars.sort.join
   if sorted1.eql?(sorted2)
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -7,4 +7,8 @@ describe('#Anagram') do
   it("Check if two words are anagrams.") do
     expect(converter.sort("eat", "tea")).to(eq("this is an anagram."))
   end
+
+  it("Check if two capitalized words are anagrams.") do
+    expect(converter.sort("Eat", "Tea")).to(eq("this is an anagram."))
+  end
 end
